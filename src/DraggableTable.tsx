b278c22ba56a3e697184508ebeecd7243d9236f2
olav-valle <olavvalle@gmail.com>
2021-10-18 16:10:08 +0200

    no major changes

--- src/DraggableTable.tsx
+++ src/DraggableTable.tsx
@@ -4,10 +4,19 @@
   https://mui-datatables-responsive-demo.skn0tt.now.sh
 */
 
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import MuiDataTable from "mui-datatables";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+  ResponderProvided,
+  DraggableProvided,
+  DroppableProvided,
+  DraggableStateSnapshot
+} from "react-beautiful-dnd";
 
 function YourCustomRowComponent(props: any) {
   const { name, cardNumber, cvc, expiry } = props;
@@ -31,68 +40,82 @@ function YourCustomRowComponent(props: any) {
       )}
     </Draggable>
   );
-
-  // return (
-  //   <div>
-  //     <h1>{name}</h1>
-  //          <p>
-  //            Number: {cardNumber} <br />
-  //            CVC: {cvc} <br />
-  //            expiry: {expiry}
-  //          </p>
-  //   </div>
-  // );
 }
 
-const creditCards = [
-  {
-    name: "Tom Tallis",
-    cardNumber: "5500005555555559",
-    cvc: "582",
-    expiry: "02/24"
-  },
-  {
-    name: "Rich Harris",
-    cardNumber: "4444444444444448",
-    cvc: "172",
-    expiry: "03/22"
-  },
-  {
-    name: "Moby Test",
-    cardNumber: "3566003566003566",
-    cvc: "230",
-    expiry: "12/25"
-  }
-];
 
-const onDragEnd = (result: any) => {};
 
 function DraggableTable() {
+  const creditCards = [
+    {
+      name: "Tom Tallis",
+      cardNumber: "5500005555555559",
+      cvc: "582",
+      expiry: "02/24"
+    },
+    {
+      name: "Rich Harris",
+      cardNumber: "4444444444444448",
+      cvc: "172",
+      expiry: "03/22"
+    },
+    {
+      name: "Moby Test",
+      cardNumber: "3566003566003566",
+      cvc: "230",
+      expiry: "12/25"
+    }
+  ];
+
+  const [localItems, setLocalItems] = useState(creditCards)
+
+  const columns = [
+    {
+      name: "name",
+      label: "Name"
+    },
+    {
+      name: "cardNumber",
+      label: "Card Number"
+    },
+    {
+      name: "cvc",
+      label: "CVC"
+    },
+    {
+      name: "expiry",
+      label: "Expiry"
+    }
+  ]
+
+
+// normally one would commit/save any order changes via an api call here...
+  const handleDragEnd = (result: DropResult, provided?: ResponderProvided) => {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    setLocalItems((prev: any) => {
+      const temp = [...prev];
+      const d = temp[result.destination!.index];
+      temp[result.destination!.index] = temp[result.source.index];
+      temp[result.source.index] = d;
+
+      return temp;
+    });
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId="droppable-1">
         {(provided, snapshot) => (
           <MuiDataTable
             title="Cards"
             data={creditCards}
-            columns={[
-              {
-                name: "name",
-                label: "Name"
-              },
-              {
-                name: "cardNumber",
-                label: "Card Number"
-              },
-              {
-                name: "cvc",
-                label: "CVC"
-              },
-              {
-                name: "expiry",
-                label: "Expiry"
-              }
-            ]}
+            columns={columns}
             options={{
               selectableRows: "none",
               responsive: "standard",
