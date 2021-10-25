be4e2aa4c8f4b4705928841d5396992c805918ce
olav-valle <olavvalle@gmail.com>
2021-10-25 12:35:49 +0200

    refactors data to TableParent todo: clean up commented code in DraggableTable

--- src/DraggableTable.tsx
+++ src/DraggableTable.tsx
@@ -4,146 +4,148 @@
   https://mui-datatables-responsive-demo.skn0tt.now.sh
 */
 
-import React, {useEffect, useState} from "react";
-import MuiDataTable from "mui-datatables";
-import {DragDropContext, Draggable, Droppable, DropResult, ResponderProvided} from "react-beautiful-dnd";
-import {DragHandle} from "@material-ui/icons";
-import {TableRow, TableCell} from "@material-ui/core"
-import {flexbox} from "@material-ui/system";
+import React, { useEffect, useState } from 'react'
+import MuiDataTable from 'mui-datatables'
+import { DragDropContext, Draggable, Droppable, DropResult, ResponderProvided } from 'react-beautiful-dnd'
+import { DragHandle } from '@material-ui/icons'
+import { TableRow, TableCell } from '@material-ui/core'
 
 function YourCustomRowComponent(props: any) {
-    const {name, cardNumber, cvc, expiry, index, colSpan} = props;
+    console.log(props)
+    const { drag, name, cardNumber, cvc, expiry, index, colSpan } = props
     //todo: fix width of draggable row to match parent table
     return (
-        <Draggable draggableId={"draggable-" + index} index={index}>
+        <Draggable draggableId={'draggable-' + index} index={index}>
             {(provided, snapshot) => (
                 <React.Fragment>
+                    {/*<TableCell*/}
+                    {/*    // colSpan={colSpan}*/}
+                    {/*>*/}
 
-                    <TableRow
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                    >
-                        <TableCell {...provided.dragHandleProps}><DragHandle/></TableCell>
-                        <TableCell>{name}</TableCell>
-                        <TableCell>
-                            Number: {cardNumber}
+                    <TableRow ref={provided.innerRef} {...provided.draggableProps}>
+                        <TableCell colSpan={1} {...provided.dragHandleProps}>
+                            <DragHandle />
+                            {drag}
                         </TableCell>
-                        <TableCell>CVC: {cvc}</TableCell>
-                        <TableCell>expiry: {expiry}</TableCell>
+                        <TableCell colSpan={1}>{name}</TableCell>
+                        <TableCell colSpan={1}>{cardNumber}</TableCell>
+                        <TableCell colSpan={1}>{cvc}</TableCell>
+                        <TableCell colSpan={1}>{expiry}</TableCell>
                     </TableRow>
+                    {/*</TableCell>*/}
                 </React.Fragment>
             )}
         </Draggable>
-    );
+    )
 }
 
+function DraggableTable(props: any) {
+    const {columns, data} = props
+    // const creditCards = [
+    //     {
+    //         name: 'Tom Tallis',
+    //         cardNumber: '5500005555555559',
+    //         cvc: '582',
+    //         expiry: '02/24',
+    //     },
+    //     {
+    //         name: 'Rich Harris',
+    //         cardNumber: '4444444444444448',
+    //         cvc: '172',
+    //         expiry: '03/22',
+    //     },
+    //     {
+    //         name: 'Moby Test',
+    //         cardNumber: '3566003566003566',
+    //         cvc: '230',
+    //         expiry: '12/25',
+    //     },
+    // ]
 
-function DraggableTable({}) {
-    const creditCards = [
-        {
-            name: "Tom Tallis",
-            cardNumber: "5500005555555559",
-            cvc: "582",
-            expiry: "02/24"
-        },
-        {
-            name: "Rich Harris",
-            cardNumber: "4444444444444448",
-            cvc: "172",
-            expiry: "03/22"
-        },
-        {
-            name: "Moby Test",
-            cardNumber: "3566003566003566",
-            cvc: "230",
-            expiry: "12/25"
-        }
-    ];
+    // const [localItems, setLocalItems] = useState(creditCards)
 
-    const [localItems, setLocalItems] = useState(creditCards)
+    // const columns = [
+    //     {
+    //         name: '',
+    //         label: '',
+    //     },
+    //     {
+    //         name: 'name',
+    //         label: 'Name',
+    //     },
+    //     {
+    //         name: 'cardNumber',
+    //         label: 'Card Number',
+    //     },
+    //     {
+    //         name: 'cvc',
+    //         label: 'CVC',
+    //     },
+    //     {
+    //         name: 'expiry',
+    //         label: 'Expiry',
+    //     },
+    // ]
 
-    const columns = [
-        {
-            name: "name",
-            label: "Name"
-        },
-        {
-            name: "cardNumber",
-            label: "Card Number"
-        },
-        {
-            name: "cvc",
-            label: "CVC"
-        },
-        {
-            name: "expiry",
-            label: "Expiry"
-        }
-    ]
+    // useEffect(() => {
+    //     setLocalItems(creditCards)
+    // }, [])
 
-    useEffect(() => {
-        setLocalItems(creditCards)
-
-    }, [])
-
-
-// normally one would commit/save any order changes via an api call here...
-    const handleDragEnd = (result: DropResult, provided?: ResponderProvided) => {
-        if (!result.destination) {
-            return;
-        }
-
-        if (result.destination.index === result.source.index) {
-            return;
-        }
-
-        setLocalItems((prev: any) => {
-            const temp = [...prev];
-            const d = temp[result.destination!.index];
-            temp[result.destination!.index] = temp[result.source.index];
-            temp[result.source.index] = d;
-
-            return temp;
-        });
-    };
+    // normally one would commit/save any order changes via an api call here...
+    // const handleDragEnd = (result: DropResult, provided?: ResponderProvided) => {
+    //     if (!result.destination) {
+    //         return
+    //     }
+    //
+    //     if (result.destination.index === result.source.index) {
+    //         return
+    //     }
+    //
+    //     setLocalItems((prev: any) => {
+    //         const temp = [...prev]
+    //         const d = temp[result.destination!.index]
+    //         temp[result.destination!.index] = temp[result.source.index]
+    //         temp[result.source.index] = d
+    //
+    //         return temp
+    //     })
+    // }
 
     return (
-        <DragDropContext onDragEnd={handleDragEnd}>
-            <Droppable droppableId="droppable-1">
-                {(provided, snapshot) => (
-                    <div ref={provided.innerRef} {...provided.droppableProps}>
+        // <DragDropContext onDragEnd={handleDragEnd}>
+        //     <Droppable droppableId="droppable-1">
+        //         {(provided, snapshot) => (
+        //             <div ref={provided.innerRef} {...provided.droppableProps}>
                         <MuiDataTable
                             title="Cards"
-                            data={localItems}
+                            data={data}
                             columns={columns}
                             options={{
-                                selectableRows: "none",
-                                responsive: "standard",
-                                customRowRender: (data, dataIndex,) => {
-                                    const [name, cardNumber, cvc, expiry] = data;
+                                selectableRows: 'none',
+                                responsive: 'standard',
+                                customRowRender: (data, dataIndex) => {
+                                    const [drag, name, cardNumber, cvc, expiry] = data
 
                                     return (
-                                        <TableRow key={dataIndex}>
-                                            <TableCell colSpan={data.length+1}>
-                                                <YourCustomRowComponent
-                                                    name={name}
-                                                    cardNumber={cardNumber}
-                                                    cvc={cvc}
-                                                    expiry={expiry}
-                                                    index={dataIndex}
-                                                />
-                                            </TableCell>
-                                        </TableRow>
-                                    );
-                                }
+                                        <YourCustomRowComponent
+                                            drag={drag}
+                                            name={name}
+                                            cardNumber={cardNumber}
+                                            cvc={cvc}
+                                            expiry={expiry}
+                                            index={dataIndex}
+                                            colSpan={data.length + 1}
+                                        />
+                                    )
+                                },
                             }}
                         />
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </DragDropContext>
-    );
+        //                 {provided.placeholder}
+        //             </div>
+        //         )}
+        //     </Droppable>
+        // </DragDropContext>
+    )
 }
 
-export default DraggableTable;
+export default DraggableTable
--- src/TableParent.tsx
+++ src/TableParent.tsx
@@ -4,15 +4,16 @@
   https://mui-datatables-responsive-demo.skn0tt.now.sh
 */
 
-import React, {useEffect, useState} from "react";
-import MuiDataTable from "mui-datatables";
-import {DragDropContext, Draggable, Droppable, DropResult, ResponderProvided} from "react-beautiful-dnd";
-import {DragHandle} from "@material-ui/icons";
+import React, { useEffect, useState } from 'react'
+import MuiDataTable from 'mui-datatables'
+import { DragDropContext, Draggable, Droppable, DropResult, ResponderProvided } from 'react-beautiful-dnd'
+import { DragHandle } from '@material-ui/icons'
+import DraggableTable from './DraggableTable'
 
 function YourCustomRowComponent(props: any) {
-    const {items, columns, index} = props;
+    const { items, columns, index } = props
     return (
-        <Droppable droppableId={"droppable-" + index}>
+        <Droppable droppableId={'droppable-' + index}>
             {(provided, snapshot) => (
                 <React.Fragment>
                     <MuiDataTable
@@ -20,14 +21,14 @@ function YourCustomRowComponent(props: any) {
                         data={items}
                         columns={columns}
                         options={{
-                            selectableRows: "none",
-                            responsive: "standard",
-                            customRowRender: (data, dataIndex,) => {
-                                const [name, cardNumber, cvc, expiry] = data;
+                            selectableRows: 'none',
+                            responsive: 'standard',
+                            customRowRender: (data, dataIndex) => {
+                                const [name, cardNumber, cvc, expiry] = data
 
                                 return (
                                     <tr key={dataIndex}>
-                                        <td colSpan={4} style={{paddingTop: "10px"}}>
+                                        <td colSpan={4} style={{ paddingTop: '10px' }}>
                                             <YourCustomRowComponent
                                                 name={name}
                                                 cardNumber={cardNumber}
@@ -37,126 +38,142 @@ function YourCustomRowComponent(props: any) {
                                             />
                                         </td>
                                     </tr>
-                                );
-                            }
+                                )
+                            },
                         }}
                     />
                 </React.Fragment>
             )}
         </Droppable>
-    );
+    )
 }
 
-
 function TableParent() {
-    const creditCards = [
-        {
-            name: "Tom Tallis",
-            cardNumber: "5500005555555559",
-            cvc: "582",
-            expiry: "02/24"
-        },
-        {
-            name: "Rich Harris",
-            cardNumber: "4444444444444448",
-            cvc: "172",
-            expiry: "03/22"
-        },
-        {
-            name: "Moby Test",
-            cardNumber: "3566003566003566",
-            cvc: "230",
-            expiry: "12/25"
-        }
-    ];
 
-    const [localItems, setLocalItems] = useState(creditCards)
+    interface IcreditCard {
+        name: string,
+        cardNumber: string,
+        cvc: string,
+        expiry: string
+    }
+
+
+    const creditCards1: IcreditCard[] = [
+        {
+            name: 'Tom Tallis',
+            cardNumber: '5500005555555559',
+            cvc: '582',
+            expiry: '02/24',
+        },
+        {
+            name: 'Rich Harris',
+            cardNumber: '4444444444444448',
+            cvc: '172',
+            expiry: '03/22',
+        },
+        {
+            name: 'Moby Test',
+            cardNumber: '3566003566003566',
+            cvc: '230',
+            expiry: '12/25',
+        },
+    ]
+
+    const creditCards2: IcreditCard[] = [
+        {
+            name: 'Tom Waits',
+            cardNumber: '5500005555555559',
+            cvc: '325',
+            expiry: '03/30',
+        },
+        {
+            name: 'Rich Gallup',
+            cardNumber: '9292929113444484',
+            cvc: '123',
+            expiry: '03/22',
+        },
+        {
+            name: 'Moby Richardson',
+            cardNumber: '0335660356005666',
+            cvc: '230',
+            expiry: '12/25',
+        },
+    ]
+
+
+
+    const creditCards: IcreditCard[][] = [
+        creditCards1,
+        creditCards2
+    ]
+
+    const [localItems, setLocalItems] = useState<IcreditCard[][] | null>(creditCards)
 
     const columns = [
         {
-            name: "name",
-            label: "Name"
+            name: '',
+            label: '',
         },
         {
-            name: "cardNumber",
-            label: "Card Number"
+            name: 'name',
+            label: 'Name',
         },
         {
-            name: "cvc",
-            label: "CVC"
+            name: 'cardNumber',
+            label: 'Card Number',
         },
         {
-            name: "expiry",
-            label: "Expiry"
-        }
+            name: 'cvc',
+            label: 'CVC',
+        },
+        {
+            name: 'expiry',
+            label: 'Expiry',
+        },
     ]
 
     useEffect(() => {
-        setLocalItems(creditCards)
-
+        if (creditCards != null) {
+            setLocalItems(creditCards)
+            console.log(localItems)
+        }
     }, [])
 
-
-// normally one would commit/save any order changes via an api call here...
+    // normally one would commit/save any order changes via an api call here...
     const handleDragEnd = (result: DropResult, provided?: ResponderProvided) => {
         if (!result.destination) {
-            return;
+            return
         }
 
         if (result.destination.index === result.source.index) {
-            return;
+            return
         }
 
         setLocalItems((prev: any) => {
-            const temp = [...prev];
-            const d = temp[result.destination!.index];
-            temp[result.destination!.index] = temp[result.source.index];
-            temp[result.source.index] = d;
+            const temp = [...prev]
+            const d = temp[result.destination!.index]
+            temp[result.destination!.index] = temp[result.source.index]
+            temp[result.source.index] = d
 
-            return temp;
-        });
-    };
-    //todo: refactor dragDropContext to parent component
-    //todo: make placeholder data sorted into parent-child-grandchild hierarchy
+            return temp
+        })
+    }
     //todo: Eval: implement onDragEnd so it works as a callback for Draggable grandchild?
+    //todo: make parent (this comp) render muitable with DraggableTable comps as rows
     //todo: make parent and grandparent collapsible
     return (
         <DragDropContext onDragEnd={handleDragEnd}>
             <Droppable droppableId="droppable-1">
                 {(provided, snapshot) => (
                     <div ref={provided.innerRef} {...provided.droppableProps}>
-                        <MuiDataTable
-                            title="Cards"
-                            data={localItems}
-                            columns={columns}
-                            options={{
-                                selectableRows: "none",
-                                responsive: "standard",
-                                customRowRender: (data, dataIndex,) => {
-                                    const [name, cardNumber, cvc, expiry] = data;
-
-                                    return (
-                                        <tr key={dataIndex}>
-                                            <td colSpan={4} style={{paddingTop: "10px"}}>
-                                                <YourCustomRowComponent
-                                                    name={name}
-                                                    cardNumber={cardNumber}
-                                                    cvc={cvc}
-                                                    expiry={expiry}
-                                                    index={dataIndex}
-                                                />
-                                            </td>
-                                        </tr>
-                                    );
-                                }
-                            }}
-                        />
+                        <DraggableTable columns={columns} data={localItems![0]}/>
+                        <DraggableTable columns={columns} data={localItems![1]}/>
                         {provided.placeholder}
                     </div>
                 )}
             </Droppable>
         </DragDropContext>
-    );
+    )
 }
 
-export default TableParent;
+export default TableParent
